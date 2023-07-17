f5281d5bc1fe4f07e7ce4a5c1906e84a3a0eac3d
Crowdbotics <engineering@crowdbotics.com>
2023-07-17 08:39:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-voice-de-102129",
+  url: "https://weathered-voice-de-102129.botics.co",
+  api: "https://weathered-voice-de-102129.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
